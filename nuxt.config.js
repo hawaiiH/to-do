43a2090ea364044c249fdbffb1b22484ce555e4b
hawaiiH <vladgusakov@gmail.com>
2020-11-18 02:15:27 +0200

    modified:   nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,9 +6,6 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   components: true,
